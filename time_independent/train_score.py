fixed normalisation issues for greyscale

time_independent/train_score.py
@@ -865,8 +865,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}\t" + (f"{torch.cuda.get_device_name(0)}" if torch.cuda.is_available() else "CPU"))
 
 """
-The parameters used here are especially sensetive
-
+The parameters used here are especially sensetive 
 """
 
 